26e82b6b4005505965edc929f79580c9e85d2ca1
Jan Trejbal <jan@trejbal.land>
2023-09-25 23:55:12 +0200

    Add GeneratorGenerationRules

---
+++ src/Aviationexam.JsonConverter.SourceGenerator/GeneratorGenerationRules.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Aviationexam.JsonConverter.SourceGenerator;
+
+public static class GeneratorGenerationRules
+{
+    public static readonly DiagnosticDescriptor UnableToParseAttribute = new(
+        $"{JsonConverterGenerator.Id}_0001",
+        "Unable to parse marking attribute",
+        "Unable to parse marking attribute {0}",
+        "Aviationexam.Core.Common.GeneratedGenericProxy",
+        DiagnosticSeverity.Error,
+        true
+    );
+}
