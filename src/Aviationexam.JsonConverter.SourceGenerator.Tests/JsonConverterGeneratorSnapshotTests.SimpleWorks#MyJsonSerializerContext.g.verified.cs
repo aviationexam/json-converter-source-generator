88240d0ac973a9c8d42b7168444ffb9bcc54a85b
Jan Trejbal <jan@trejbal.land>
2023-09-26 17:26:21 +0200

    Add JsonConverterGeneratorSnapshotTests.SimpleWorks#MyJsonSerializerContext.g

---
+++ src/Aviationexam.JsonConverter.SourceGenerator.Tests/JsonConverterGeneratorSnapshotTests.SimpleWorks#MyJsonSerializerContext.g.verified.cs
@@ -0,0 +1,22 @@
+﻿//HintName: MyJsonSerializerContext.g.cs
+using System.Collections.Generic;
+
+namespace ApplicationNamespace;
+
+public partial class MyJsonSerializerContext
+{
+    protected static IReadOnlyCollection<System.Text.Json.Serialization.JsonConverter> GetPolymorphicConverters() => new System.Text.Json.Serialization.JsonConverter[]
+    {
+        new BaseContractJsonPolymorphicConverter(),
+    };
+
+    protected static void UsePolymorphicConverters(
+        ICollection<System.Text.Json.Serialization.JsonConverter> optionsConverters
+    )
+    {
+        foreach (var converter in GetPolymorphicConverters())
+        {
+            optionsConverters.Add(converter);
+        }
+    }
+}
