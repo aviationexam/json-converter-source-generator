05e395f11ed24aa3103060720ea9c794836b0a6e
Jan Trejbal <jan@trejbal.land>
2023-09-27 14:39:45 +0200

    Use StringSyntax

--- src/Aviationexam.JsonConverter.SourceGenerator.Tests/TestHelper.cs
+++ src/Aviationexam.JsonConverter.SourceGenerator.Tests/TestHelper.cs
@@ -2,6 +2,7 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection;
 using System.Text.Json;
@@ -12,15 +13,17 @@ namespace Aviationexam.JsonConverter.SourceGenerator.Tests;
 
 public static class TestHelper
 {
-    public static Task Verify(string source)
+    public static Task Verify(
+        [StringSyntax("csharp")] params string[] source
+    )
     {
         // Parse the provided string into a C# syntax tree
-        var syntaxTree = CSharpSyntaxTree.ParseText(source);
+        var syntaxTrees = source.Select(x => CSharpSyntaxTree.ParseText(x)).ToArray();
 
         // Create a Roslyn compilation for the syntax tree.
         var compilation = CSharpCompilation.Create(
             assemblyName: "Tests",
-            syntaxTrees: new[] { syntaxTree },
+            syntaxTrees: syntaxTrees,
             references: new[]
                 {
                     typeof(object).Assembly.Location,
