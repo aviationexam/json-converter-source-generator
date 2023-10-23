bd068ba2aa8668172e052775c49b1ed8ea42ce34
Jan Trejbal <jan@trejbal.land>
2023-10-23 10:51:43 +0200

    Add SerializeEnumWorks

---
+++ src/Aviationexam.GeneratedJsonConverters.SourceGenerator.Target.Tests/EnumSerializationTests.cs
@@ -0,0 +1,30 @@
+﻿using Aviationexam.GeneratedJsonConverters.SourceGenerator.Target.Contracts;
+using System.Text.Json;
+using Xunit;
+
+namespace Aviationexam.GeneratedJsonConverters.SourceGenerator.Target.Tests;
+
+public class EnumSerializationTests
+{
+    [Theory]
+    [InlineData(EBackingEnum.A, "0")]
+    [InlineData(EBackingEnum.B, "1")]
+    [InlineData(EConfiguredPropertyEnum.A, "\"C\"")]
+    [InlineData(EConfiguredPropertyEnum.B, "\"D\"")]
+    [InlineData(EMyEnum.A, "\"A\"")]
+    [InlineData(EMyEnum.B, "\"B\"")]
+    [InlineData(EPropertyEnum.C, "\"C\"")]
+    [InlineData(EPropertyEnum.D, "\"D\"")]
+    [InlineData(EPropertyWithBackingEnum.E, "\"E\"")]
+    [InlineData(EPropertyWithBackingEnum.F, "\"F\"")]
+    public void SerializeEnumWorks(object enumValue, string expectedValue)
+    {
+        var serializedValue = JsonSerializer.Serialize(
+            enumValue,
+            enumValue.GetType(),
+            MyJsonSerializerContext.Default.Options
+        );
+
+        Assert.Equal(expectedValue, serializedValue);
+    }
+}
--- src/Aviationexam.GeneratedJsonConverters.SourceGenerator.Target/MyJsonSerializerContext.cs
+++ src/Aviationexam.GeneratedJsonConverters.SourceGenerator.Target/MyJsonSerializerContext.cs
@@ -14,10 +14,16 @@ namespace Aviationexam.GeneratedJsonConverters.SourceGenerator.Target;
 [JsonSerializable(typeof(AnotherLeafContract))]
 [JsonSerializable(typeof(BaseContractWithCustomDelimiter))]
 [JsonSerializable(typeof(LeafContractWithCustomDelimiter))]
+[JsonSerializable(typeof(EBackingEnum))]
+[JsonSerializable(typeof(EConfiguredPropertyEnum))]
+[JsonSerializable(typeof(EMyEnum))]
+[JsonSerializable(typeof(EPropertyEnum))]
+[JsonSerializable(typeof(EPropertyWithBackingEnum))]
 public partial class MyJsonSerializerContext : JsonSerializerContext
 {
     static MyJsonSerializerContext()
     {
         UsePolymorphicConverters(s_defaultOptions.Converters);
+        UseEnumConverters(s_defaultOptions.Converters);
     }
 }
