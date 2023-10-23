a93f72d7f25b9b5d02ae142f564740402fd01500
Jan Trejbal <jan@trejbal.land>
2023-10-23 10:54:24 +0200

    Add DeserializeEnumWorks

--- src/Aviationexam.GeneratedJsonConverters.SourceGenerator.Target.Tests/EnumSerializationTests.cs
+++ src/Aviationexam.GeneratedJsonConverters.SourceGenerator.Target.Tests/EnumSerializationTests.cs
@@ -27,4 +27,28 @@ public class EnumSerializationTests
 
         Assert.Equal(expectedValue, serializedValue);
     }
+
+    [Theory]
+    [InlineData(EBackingEnum.A, "0")]
+    [InlineData(EBackingEnum.B, "1")]
+    [InlineData(EConfiguredPropertyEnum.A, "\"C\"")]
+    [InlineData(EConfiguredPropertyEnum.B, "\"D\"")]
+    [InlineData(EMyEnum.A, "\"A\"")]
+    [InlineData(EMyEnum.B, "\"B\"")]
+    [InlineData(EPropertyEnum.C, "\"C\"")]
+    [InlineData(EPropertyEnum.D, "\"D\"")]
+    [InlineData(EPropertyWithBackingEnum.E, "\"E\"")]
+    [InlineData(EPropertyWithBackingEnum.F, "\"F\"")]
+    [InlineData(EPropertyWithBackingEnum.E, "0")]
+    [InlineData(EPropertyWithBackingEnum.F, "1")]
+    public void DeserializeEnumWorks(object expectedValue, string sourceJson)
+    {
+        var serializedValue = JsonSerializer.Deserialize(
+            sourceJson,
+            expectedValue.GetType(),
+            MyJsonSerializerContext.Default.Options
+        );
+
+        Assert.Equal(expectedValue, serializedValue);
+    }
 }
