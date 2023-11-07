dea3ce5322b9bce30a85cb48887d54e8b78a6caf
Jan Trejbal <jan@trejbal.land>
2023-11-07 13:25:14 +0100

    Use generic JsonDerivedType in Target tests

--- src/Aviationexam.GeneratedJsonConverters.SourceGenerator.Target/Contracts/BaseContract.cs
+++ src/Aviationexam.GeneratedJsonConverters.SourceGenerator.Target/Contracts/BaseContract.cs
@@ -5,6 +5,7 @@ namespace Aviationexam.GeneratedJsonConverters.SourceGenerator.Target.Contracts;
 [JsonPolymorphic]
 [JsonDerivedType(typeof(LeafContract), typeDiscriminator: nameof(LeafContract))]
 [JsonDerivedType(typeof(AnotherLeafContract), typeDiscriminator: 2)]
+[JsonDerivedType<GenericLeafContract>(typeDiscriminator: nameof(GenericLeafContract))]
 public abstract class BaseContract
 {
     public int BaseProperty { get; set; }
---
+++ src/Aviationexam.GeneratedJsonConverters.SourceGenerator.Target/Contracts/GenericLeafContract.cs
@@ -0,0 +1,6 @@
+namespace Aviationexam.GeneratedJsonConverters.SourceGenerator.Target.Contracts;
+
+public sealed class GenericLeafContract : BaseContract
+{
+    public int Property { get; set; }
+}
