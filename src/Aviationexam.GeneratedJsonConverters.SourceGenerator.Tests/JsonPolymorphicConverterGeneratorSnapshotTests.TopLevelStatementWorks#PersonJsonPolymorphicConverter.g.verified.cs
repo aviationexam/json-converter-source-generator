d88fff5199a5b1e1cef026372bea88c0ac370103
Jan Trejbal <jan@trejbal.land>
2023-11-07 13:56:38 +0100

    Fix JsonDerivedTypeAttribute parsing (#33)
    * Fix JsonDerivedTypeAttributeParser
    
    * Fix generic JsonDerivedTypeAttribute docblock

---
+++ src/Aviationexam.GeneratedJsonConverters.SourceGenerator.Tests/JsonPolymorphicConverterGeneratorSnapshotTests.TopLevelStatementWorks#PersonJsonPolymorphicConverter.g.verified.cs
@@ -0,0 +1,35 @@
+﻿//HintName: PersonJsonPolymorphicConverter.g.cs
+#nullable enable
+
+namespace ;
+
+internal class PersonJsonPolymorphicConverter : Aviationexam.GeneratedJsonConverters.PolymorphicJsonConvertor<Person>
+{
+    protected override System.ReadOnlySpan<byte> GetDiscriminatorPropertyName() => "$type"u8;
+
+    protected override System.Type GetTypeForDiscriminator(
+        Aviationexam.GeneratedJsonConverters.IDiscriminatorStruct discriminator
+    ) => discriminator switch
+    {
+        Aviationexam.GeneratedJsonConverters.DiscriminatorStruct<string> { Value: "Student" } => typeof(Student),
+        Aviationexam.GeneratedJsonConverters.DiscriminatorStruct<string> { Value: "Teacher" } => typeof(Teacher),
+
+        _ => throw new System.ArgumentOutOfRangeException(nameof(discriminator), discriminator, null),
+    };
+
+    protected override Aviationexam.GeneratedJsonConverters.IDiscriminatorStruct GetDiscriminatorForType(
+        System.Type type
+    )
+    {
+        if (type == typeof(Student))
+        {
+            return new Aviationexam.GeneratedJsonConverters.DiscriminatorStruct<string>("Student");
+        }
+        if (type == typeof(Teacher))
+        {
+            return new Aviationexam.GeneratedJsonConverters.DiscriminatorStruct<string>("Teacher");
+        }
+
+        throw new System.ArgumentOutOfRangeException(nameof(type), type, null);
+    }
+}
