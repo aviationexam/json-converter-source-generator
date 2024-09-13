b5023a2ee9f2e2864f4e6443c6040cd20308c6be
Jan Trejbal <jan@trejbal.land>
2024-09-13 17:47:16 +0200

    Add SimpleRecordWorks

---
+++ src/Aviationexam.GeneratedJsonConverters.SourceGenerator.Tests/JsonPolymorphicConverterGeneratorSnapshotTests.SimpleRecordWorks#MyJsonSerializerContext.g.verified.cs
@@ -0,0 +1,22 @@
+﻿//HintName: MyJsonSerializerContext.g.cs
+#nullable enable
+
+namespace ApplicationNamespace;
+
+public partial class MyJsonSerializerContext
+{
+    public static System.Collections.Generic.IReadOnlyCollection<System.Text.Json.Serialization.JsonConverter> GetPolymorphicConverters() => new System.Text.Json.Serialization.JsonConverter[]
+    {
+        new ApplicationNamespace.BaseContractJsonPolymorphicConverter(),
+    };
+
+    public static void UsePolymorphicConverters(
+        System.Collections.Generic.ICollection<System.Text.Json.Serialization.JsonConverter> optionsConverters
+    )
+    {
+        foreach (var converter in GetPolymorphicConverters())
+        {
+            optionsConverters.Add(converter);
+        }
+    }
+}
