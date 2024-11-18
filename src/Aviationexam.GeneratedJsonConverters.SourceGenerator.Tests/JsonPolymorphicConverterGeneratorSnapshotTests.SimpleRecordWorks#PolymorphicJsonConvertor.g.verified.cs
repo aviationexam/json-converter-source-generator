45107c8b9bcaa7ea6a5428f1dd6e1e3afa45236f
Jan Trejbal <jan@trejbal.land>
2024-11-18 19:52:52 +0100

    Update snapshots

--- src/Aviationexam.GeneratedJsonConverters.SourceGenerator.Tests/JsonPolymorphicConverterGeneratorSnapshotTests.SimpleRecordWorks#PolymorphicJsonConvertor.g.verified.cs
+++ src/Aviationexam.GeneratedJsonConverters.SourceGenerator.Tests/JsonPolymorphicConverterGeneratorSnapshotTests.SimpleRecordWorks#PolymorphicJsonConvertor.g.verified.cs
@@ -48,7 +48,7 @@ internal abstract class PolymorphicJsonConvertor<T> : JsonConverter<T> where T :
 
         var type = GetTypeForDiscriminator(typeDiscriminator);
 
-        return (T?) jsonDocument.Deserialize(type, options);
+        return (T?) jsonDocument.Deserialize(options.GetTypeInfo(type));
     }
 
     public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
@@ -79,7 +79,7 @@ internal abstract class PolymorphicJsonConvertor<T> : JsonConverter<T> where T :
             }
 
             writer.WritePropertyName(p.Name);
-            JsonSerializer.Serialize(writer, p.Get(value), p.PropertyType, options);
+            JsonSerializer.Serialize(writer, p.Get(value), options.GetTypeInfo(p.PropertyType));
         }
 
         writer.WriteEndObject();
