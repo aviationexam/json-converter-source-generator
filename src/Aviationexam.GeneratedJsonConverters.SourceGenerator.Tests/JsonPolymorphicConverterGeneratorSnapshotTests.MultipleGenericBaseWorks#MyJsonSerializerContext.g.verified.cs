ce6b0c89856f9402ab4d8cb662dee7bf27841f33
Jan Trejbal <jan@trejbal.land>
2026-03-16 15:48:11 +0100

    Add MultipleGenericBaseWorks

---
+++ src/Aviationexam.GeneratedJsonConverters.SourceGenerator.Tests/JsonPolymorphicConverterGeneratorSnapshotTests.MultipleGenericBaseWorks#MyJsonSerializerContext.g.verified.cs
@@ -0,0 +1,29 @@
+﻿//HintName: MyJsonSerializerContext.g.cs
+#nullable enable
+
+namespace ApplicationNamespace;
+
+public partial class MyJsonSerializerContext
+{
+    public static System.Collections.Generic.IReadOnlyCollection<System.Text.Json.Serialization.JsonConverter> GetPolymorphicConverters() => new System.Text.Json.Serialization.JsonConverter[]
+    {
+        new ApplicationNamespace.BaseContractOfAJsonPolymorphicConverter(),
+        new ApplicationNamespace.BaseContractOfBJsonPolymorphicConverter(),
+    };
+
+    public static void UsePolymorphicConverters(
+        System.Collections.Generic.ICollection<System.Text.Json.Serialization.JsonConverter> optionsConverters
+    )
+    {
+        foreach (var converter in GetPolymorphicConverters())
+        {
+            optionsConverters.Add(converter);
+        }
+    }
+
+    public static System.Collections.Generic.IReadOnlyCollection<System.Action<System.Text.Json.Serialization.Metadata.JsonTypeInfo>> GetPolymorphicJsonTypeInfoConfigurations() => new System.Action<System.Text.Json.Serialization.Metadata.JsonTypeInfo>[]
+    {
+        ApplicationNamespace.BaseContractOfAJsonPolymorphicConverter.ConfigureJsonTypeInfo,
+        ApplicationNamespace.BaseContractOfBJsonPolymorphicConverter.ConfigureJsonTypeInfo,
+    };
+}
